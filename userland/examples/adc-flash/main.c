6dfa3866126e0a9d0ee2bcd5b2d2104b1156c330
Holly Chiang <hchiang1@stanford.edu>
2018-03-14 16:17:07 -0700

    undo changes to adc-flash app; flash cannot be serviced through interrupts

--- userland/examples/adc-flash/main.c
+++ userland/examples/adc-flash/main.c
@@ -15,40 +15,6 @@ static void write_done(int length,
     done = true;
 }
 
-static void timer_fired(__attribute__ ((unused)) int arg0,
-                        __attribute__ ((unused)) int arg1,
-                        __attribute__ ((unused)) int arg2,
-                        __attribute__ ((unused)) void* ud) {
-    
-    // Take ADC sample
-    uint8_t channel = 0;
-    uint32_t freq = 150000;
-    uint32_t length = 10;
-    uint16_t buf[length];
-    int err = adc_sample_buffer_sync(channel, freq, buf, length);
-
-    // Output adc results
-    if (err < 0) {
-        printf("Error sampling ADC: %d\n", err);
-    }
-    else {
-        printf("\t[ ");
-        for (uint32_t i = 0; i < length; i++) {
-            // convert to millivolts
-            writebuf[i] = (buf[i] * 3300) / 4095;
-            printf("%u ", writebuf[i]);
-        }
-        printf("]\n ");
-    }
-    
-    // Write to flash
-    done = false;
-    int ret  = nonvolatile_storage_internal_write(0, length);
-    if (ret != 0) printf("ERROR calling write\n");
-    yield_for(&done);
-} 
-
-
 int main(void) {
 
     printf("Begin test\n");
@@ -61,9 +27,38 @@ int main(void) {
     if (ret != 0) printf("ERROR setting write done callback\n");
     
 
-    static tock_timer_t timer;
-    timer_every(1000, timer_fired, NULL, &timer);
+    uint8_t channel = 0;
+    uint32_t freq = 150000;
+    uint32_t length = 10;
+    uint16_t buf[length];
 
+    while(1){
+        int err = adc_sample_buffer_sync(channel, freq, buf, length);
+
+        // Output adc results
+        if (err < 0) {
+            printf("Error sampling ADC: %d\n", err);
+        }
+        else {
+            printf("\t[ ");
+            for (uint32_t i = 0; i < length; i++) {
+                // convert to millivolts
+                writebuf[i] = (buf[i] * 3300) / 4095;
+                printf("%u ", writebuf[i]);
+            }
+            printf("]\n ");
+        }
+        
+        // Write to flash
+        done = false;
+        int ret  = nonvolatile_storage_internal_write(0, length);
+        if (ret != 0) printf("ERROR calling write\n");
+        yield_for(&done);
+
+        //Can't use timer_every because undefined behavior can occur if
+        // flash is serviced through interrupts (pg. 265)
+        delay_ms(1000);
+    }
 
 }
 
