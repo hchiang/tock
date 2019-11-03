b55fea3ce8f921a042d7e012a5841f98cdfaa0e2
Holly Chiang <hchiang@DN0a1eaf13.SUNet>
2019-11-03 11:51:00 -0800

    removed radio setting higher clock for gpio

--- kernel/src/hil/gpio.rs
+++ kernel/src/hil/gpio.rs
@@ -54,9 +54,6 @@ pub trait Pin {
 
     /// Disable the interrupt for the GPIO pin.
     fn disable_interrupt(&self);
-
-    /// Set the minimum clock rate for clock management
-    fn set_clock_rate(&self, clock_rate: u32);
 }
 
 /// Interface for users of synchronous GPIO. In order
